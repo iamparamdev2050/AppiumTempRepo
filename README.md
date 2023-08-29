1340f1d0adb43c4baf1e2d55a85a7eb7d76f578a
officialtpss <officialtpss@gmail.com>
2023-08-29 15:24:56 +0530

    Ios initial setup

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# AppiumTempRepo
+# -appiumExample
--- src/test/java/DemoTest.java
+++ src/test/java/DemoTest.java
@@ -1,8 +1,6 @@
+
 import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.MobileElement;
-import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterClass;
@@ -17,8 +15,8 @@ import io.appium.java_client.ios.IOSDriver;
 
 public class DemoTest {
 
-    AppiumDriver<MobileElement> driver;
-    String mHost="http://localhost:4723/wd/hub";
+    WebDriver driver;
+    String mHost="http://localhost:4723";
 
     @BeforeClass
     public void setUp() throws MalformedURLException {
@@ -28,24 +26,13 @@ public class DemoTest {
         DesiredCapabilities iosCapabilities = new DesiredCapabilities();
         iosCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "iOS");
         iosCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "15.4");
-        iosCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Iphone SE");
-        System.out.println("<<<<<<<<<<<<<<<<<<<<<<Test Start - 2>>>>>>>>>>>>>>>>>>>>");
-        iosCapabilities.setCapability(MobileCapabilityType.APP, System.getProperty("user.dir")+"/apps/testios.app");
-        System.out.println("<<<<<<<<<<<<<<<<<<<<<<Test Start - 3>>>>>>>>>>>>>>>>>>>>");
-        iosCapabilities.setCapability("automationName", "XCUITest"); // Automation name for iOS
-        driver = new IOSDriver<MobileElement>(new URL(mHost), iosCapabilities);
-        System.out.println("<<<<<<<<<<<<<<<<<<<<<<Test Start - 4>>>>>>>>>>>>>>>>>>>>");
-
-
-
-        // Create an Android driver using Android desired capabilities
-//       DesiredCapabilities androidCapabilities = new DesiredCapabilities();
-//        androidCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
-//        androidCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "11");
-//        androidCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Nexus 6");
-//        androidCapabilities.setCapability(MobileCapabilityType.APP, System.getProperty("user.dir")+"/apps/app-debug.apk");
-//        androidCapabilities.setCapability("automationName", "UiAutomator2"); // Automation name for Android
-//        driver = new AndroidDriver<>(new URL(mHost), androidCapabilities);
+        iosCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "TestiPhoneX");
+        System.out.println("<<<<<<<<<<<<<<<<<<<<<<Set .app file>>>>>>>>>>>>>>>>>>>>");
+        iosCapabilities.setCapability(MobileCapabilityType.APP, System.getProperty("user.dir")+"/apps/SampleiOS.app");
+        iosCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "XCUITest");
+        System.out.println("<<<<<<<<<<<<<<<<<<<<<<Set iosCapabilities >>>>>>>>>>>>>>>>>>>>");
+        driver= new IOSDriver(new URL(mHost), iosCapabilities);
+        System.out.println("<<<<<<<<<<<<<<<<<<<<<<Set iosCapabilities Done >>>>>>>>>>>>>>>>>>>>");
 
     }
 
