d5da4ed75a34ec5a021e15f6ad8f143e81dc53bd
iamparamdev2050 <91934080+iamparamdev2050@users.noreply.github.com>
2023-08-31 16:52:35 +0530

    Update RunAppiumServer.sh

--- scripts/RunAppiumServer.sh
+++ scripts/RunAppiumServer.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -ex
-npm uninstall -g appium
 npm install -g appium
 # appium driver install uiautomator2
 appium driver install xcuitest
+appium driver run xcuitest build-wda
 appium driver install --source=npm appium-flutter-driver
 
 appium -v
