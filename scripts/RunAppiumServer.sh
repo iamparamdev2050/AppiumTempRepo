613011b2bf3499789b39ef4dcc969ec1035a789f
iamparamdev2050 <91934080+iamparamdev2050@users.noreply.github.com>
2023-09-01 08:35:44 +0530

    Update RunAppiumServer.sh

--- scripts/RunAppiumServer.sh
+++ scripts/RunAppiumServer.sh
@@ -3,7 +3,7 @@ set -ex
 npm install -g appium
 # appium driver install uiautomator2
 appium driver install xcuitest
-appium-doctor --ios
+appium doctor --ios
 appium driver install --source=npm appium-flutter-driver
 
 appium -v
