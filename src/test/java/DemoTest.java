bf430a5756a991445ce66d7cf56de767e6c26ee2
iamparamdev2050 <91934080+iamparamdev2050@users.noreply.github.com>
2023-09-01 12:10:14 +0530

    Update DemoTest.java

--- src/test/java/DemoTest.java
+++ src/test/java/DemoTest.java
@@ -23,37 +23,37 @@ public class DemoTest {
     public void setUp() throws MalformedURLException {
         System.out.println("<<<<<<<<<<<<<<<<<<<<<<Test Start>>>>>>>>>>>>>>>>>>>>");
 
-        // DesiredCapabilities iosCapabilities = new DesiredCapabilities();
-        // iosCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "iOS");
-        // iosCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "14.2");
-        // iosCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhoneX");
-        // System.out.println("<<<<<<<<<<<<<<<<<<<<<<Set .app file>>>>>>>>>>>>>>>>>>>>");
-        // iosCapabilities.setCapability(MobileCapabilityType.APP, System.getProperty("user.dir")+"/apps/Runner.app");
-        // iosCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "XCUItest");
-        // iosCapabilities.setCapability(MobileCapabilityType.NO_RESET, "true");
+        DesiredCapabilities iosCapabilities = new DesiredCapabilities();
+        iosCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "iOS");
+        iosCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "14.2");
+        iosCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhoneX");
+        System.out.println("<<<<<<<<<<<<<<<<<<<<<<Set .app file>>>>>>>>>>>>>>>>>>>>");
+        iosCapabilities.setCapability(MobileCapabilityType.APP, System.getProperty("user.dir")+"/apps/Runner.app");
+        iosCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "XCUItest");
+        iosCapabilities.setCapability(MobileCapabilityType.NO_RESET, "true");
 
-        // iosCapabilities.setCapability("wdaStartupRetries", "4");
-        // iosCapabilities.setCapability("iosInstallPause","8000" );
-        // iosCapabilities.setCapability("wdaStartupRetryInterval", "20000");
-        // iosCapabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 3600);
+        iosCapabilities.setCapability("wdaStartupRetries", "4");
+        iosCapabilities.setCapability("iosInstallPause","8000" );
+        iosCapabilities.setCapability("wdaStartupRetryInterval", "20000");
+        iosCapabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 3600);
 
-        // System.out.println("<<<<<<<<<<<<<<<<<<<<<<Set iosCapabilities start>>>>>>>>>>>>>>>>>>>>");
-        // driver= new IOSDriver(new URL(mHost), iosCapabilities);
-        // System.out.println("<<<<<<<<<<<<<<<<<<<<<<Set iosCapabilities Done >>>>>>>>>>>>>>>>>>>>");
+        System.out.println("<<<<<<<<<<<<<<<<<<<<<<Set iosCapabilities start>>>>>>>>>>>>>>>>>>>>");
+        driver= new IOSDriver(new URL(mHost), iosCapabilities);
+        System.out.println("<<<<<<<<<<<<<<<<<<<<<<Set iosCapabilities Done >>>>>>>>>>>>>>>>>>>>");
 
 
-        DesiredCapabilities androidCapabilities = new DesiredCapabilities();
-        androidCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
-        androidCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Nexus 6"); // Replace with your device's name
-        androidCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "11");
-        androidCapabilities.setCapability(MobileCapabilityType.APP, System.getProperty("user.dir")+"/apps/GusiAndroid.apk");
-        System.out.println("<<<<<<<<<<<<<<<<<<<<<<Added .apk file>>>>>>>>>>>>>>>>>>>>");
-        androidCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
-        androidCapabilities.setCapability(MobileCapabilityType.NO_RESET, "true");
+        // DesiredCapabilities androidCapabilities = new DesiredCapabilities();
+        // androidCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        // androidCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Nexus 6"); // Replace with your device's name
+        // androidCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "11");
+        // androidCapabilities.setCapability(MobileCapabilityType.APP, System.getProperty("user.dir")+"/apps/GusiAndroid.apk");
+        // System.out.println("<<<<<<<<<<<<<<<<<<<<<<Added .apk file>>>>>>>>>>>>>>>>>>>>");
+        // androidCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+        // androidCapabilities.setCapability(MobileCapabilityType.NO_RESET, "true");
 
-        System.out.println("<<<<<<<<<<<<<<<<<<<<<<Set androidCapabilities start>>>>>>>>>>>>>>>>>>>>");
-        driver= new AndroidDriver(new URL(mHost), androidCapabilities);
-        System.out.println("<<<<<<<<<<<<<<<<<<<<<<Set androidCapabilities Done >>>>>>>>>>>>>>>>>>>>");
+        // System.out.println("<<<<<<<<<<<<<<<<<<<<<<Set androidCapabilities start>>>>>>>>>>>>>>>>>>>>");
+        // driver= new AndroidDriver(new URL(mHost), androidCapabilities);
+        // System.out.println("<<<<<<<<<<<<<<<<<<<<<<Set androidCapabilities Done >>>>>>>>>>>>>>>>>>>>");
 
 
     }
